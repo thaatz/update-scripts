03daadcb43e1bd76d0da75792891d19172838dff
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2020-06-23 12:35:37 -0400

    git client patcher now updates explorer context menu items

--- gitkrakenpatcher.bat
+++ gitkrakenpatcher.bat
@@ -1,5 +1,6 @@
 @echo off
 pushd "%~dp0" 2>NUL
+echo. [*] Updating prerequisites . . .
 REM requires nodejs, yarn, nircmd, git?,
 REM choco upgrade nircmd -y
 choco upgrade nodejs -y
@@ -36,9 +37,22 @@ for /F "delims= eol=|" %%f in ('
 )
 
 REM CHANGE THE START MENU SHORTCUT SO IT NO LONGER POINTS TO UPDATE.EXE
+echo.
+echo. [*] Updating shortcuts . . .
 del "%appdata%\Microsoft\Windows\Start Menu\Programs\Axosoft, LLC\GitKraken.lnk" >nul 2>nul
 REM https://nircmd.nirsoft.net/shortcut.html
 nircmd shortcut "%localappdata%\gitkraken\%gitkrakenversion%\gitkraken.exe" "~$folder.programs$\Axosoft, LLC" "GitKraken"
 
+REM CHANGE THE REGISTRY VALUES FOR THE RIGHT CLICK EXPLORER CONTEXT MENU
+echo.
+echo. [*] Updating right-click context menu . . .
+reg add HKEY_CLASSES_ROOT\directory\background\shell\GitKraken\command /f /d "\"%localappdata%\gitkraken\%gitkrakenversion%\gitkraken.exe\" -p \"%%V\""
+reg add HKEY_CLASSES_ROOT\directory\shell\GitKraken\command /f /d "\"%localappdata%\gitkraken\%gitkrakenversion%\gitkraken.exe\" -p \"%%1\""
+REM use the following paths to revert context menu changes
+REM "\"C:\\Users\\thomanoon.hongsmatip\\AppData\\Local\\gitkraken\\update.exe\" --processStart=gitkraken.exe --process-start-args=\"-p \\\"%V\\\"\""
+REM "\"C:\\Users\\thomanoon.hongsmatip\\AppData\\Local\\gitkraken\\update.exe\" --processStart=gitkraken.exe --process-start-args=\"-p \\\"%1\\\"\""
+
+echo.
+echo. [!] Finished
 pause
 popd
