d0efed8e60b3d931bd42fb6ce96976b31b36fced
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2020-08-10 13:30:24 -0400

    hotfix directorysetup script

--- common/directorysetup.bat
+++ common/directorysetup.bat
@@ -4,7 +4,7 @@ if not exist %shortcutsfolder% mkdir %shortcutsfolder%
 if not exist %shortcutsfolder%\av mkdir %shortcutsfolder%\av
 if not exist %shortcutsfolder%\hdd mkdir %shortcutsfolder%\hdd
 
-pushd %shortcutsfolder%
+REM pushd %shortcutsfolder%
 REM echo checking for updates . . .
 REM choco outdated>temp.txt
 REM type "temp.txt" | findstr /C:"Chocolatey has determined 0 package(s) are outdated">nul
