412bc9a532f77024d1f0af8dc810d13c5896e270
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2021-01-24 22:55:24 -0500

    added zerotier one

--- chocolatey-laptop.bat
+++ chocolatey-laptop.bat
@@ -54,6 +54,7 @@ choco upgrade pia -y
 choco upgrade tor-browser -y
 move /y "%public%\Desktop\Tor Browser.lnk" %shortcutsfolder% >nul 2>nul
 move /y "%userprofile%\Desktop\Tor Browser.lnk" %shortcutsfolder% >nul 2>nul
+choco upgrade zerotier-one -y
 
 REM PRODUCTIVITY
 call common\commonproductivity.bat REM firefox, notepadplusplus, sumatrapdf, vlc
